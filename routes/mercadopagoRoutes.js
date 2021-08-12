be72da96718cc10173fda763a6f5b0cb1102b588
ominukamauro <ominuka.mauro@gmail.com>
2021-08-12 10:29:14 -0300

    deploy to heroku

--- routes/mercadopagoRoutes.js
+++ routes/mercadopagoRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { createPreference, feedback, ipn } from '../controllers/mercadopagoController.js'
+import { createPreference, ipn } from '../controllers/mercadopagoController.js'
 const Router = express.Router()
 
 Router.post('/mercadopago/create_preference', createPreference)
 Router.post('/mercadopago/ipn/:type/:data.id', ipn)
-Router.get('/feedback', feedback)
-
+/* Router.get('/feedback', feedback)
+ */
 export default Router
