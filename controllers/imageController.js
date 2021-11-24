bd362dc20b4de37656b3ef485784139a45987a14
OmiMauro <ominuka.mauro@gmail.com>
2021-11-24 14:39:12 -0300

    LigthBox and deploy

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -24,7 +24,7 @@ const getAllImages = async (req, res) => {
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page - 1) * limit
-    const images = await Image.find({}).skip(skip).limit(limit)
+    const images = await Image.find({}, { user: 0 }).skip(skip).limit(limit)
     return res.status(200).json({ images, msg: 'Fetched images' })
   } catch (error) {
     return res.status(500).json(error)
