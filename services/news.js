df55e39ad253e4cf697ff3cf05f3ca96f0a69a14
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-07 18:33:27 -0300

    fix: change keyword new by news. Reserved Word

--- services/news.js
+++ services/news.js
@@ -3,11 +3,11 @@ import newsRepository from '../repositories/news.js'
 const getById = async (id) => {
   return await newsRepository.getById(id)
 }
-const update = async (id, new) => {
-  return await newsRepository.update(id, new)
+const update = async (id, news) => {
+  return await newsRepository.update(id, news)
 }
-const create = async (new) => {
-  return await newsRepository.create(new)
+const create = async (news) => {
+  return await newsRepository.create(news)
 }
 const getAll = async () => {
   return await newsRepository.getAll()
