a83a0e1f14ad7b1e8cb2471bdd04ade5722eff09
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-08 16:22:30 -0300

    Added middlwares for validations input of organization

--- middlewares/organizations-validation.js
+++ middlewares/organizations-validation.js
@@ -1,2 +1,38 @@
 import { executeValidation } from './validation-index'
-import { check } from 'express-validator'
+import { check, param } from 'express-validator'
+
+const name = check('name', 'Ingresar el nombre')
+  .notEmpty()
+  .trim()
+const image = check('image', 'Ingresar la imagen o avatar')
+  .notEmpty()
+  .trim()
+const phone = check('phone', 'Ingresar el celular')
+  .notEmpty()
+  .trim()
+const email = check('email', 'Ingresar el email')
+  .notEmpty()
+  .trim()
+  .isEmail()
+const welcomeText = check('welcomeText', 'Ingresar un texto de bienvenida')
+  .trim()
+  .isString()
+const aboutUs = check('aboutUs', 'Ingresar una descripcion sobre la organizacion o grupo')
+  .trim()
+  .isString()
+
+const urlFacebook = check('urlfacebook', 'Ingresar la URL del Facebook')
+  .trim()
+const urlInstragram = check('urlinstragram', 'Ingresar la URL del Instagram')
+  .trim()
+const urlWhatsapp = check('urlwhatsapp', 'Ingresar la URL del Whatsapp')
+  .trim()
+const id = param('id', 'Ingresar el ID como parametro')
+  .isMongoId()
+
+const createValidation = [name, image, phone, email, welcomeText, aboutUs, urlFacebook, urlInstragram, urlWhatsapp, executeValidation]
+
+const removeValidation = [id, executeValidation]
+const updateValidation = [id, name, image, phone, email, welcomeText, aboutUs, urlFacebook, urlInstragram, urlWhatsapp, executeValidation]
+
+export { createValidation, removeValidation, updateValidation }
