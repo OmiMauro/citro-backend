b5df3f97a6d9febc8d9f71ed68f573fcb099cb6b
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-08 11:03:08 -0300

    Added missing code for members

--- controllers/members.js
+++ controllers/members.js
@@ -5,5 +5,5 @@ const update = async (req, res) => {}
 const remove = async (req, res) => {}
 const getAll = async (req, res) => {}
 const getById = async (req, res) => {}
-
-export { create, update, remove, getAll, getById }
+const membersControlllers = { create, update, remove, getAll, getById }
+export default membersControlllers
--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,11 @@
 import Express from 'express'
+import membersControlllers from '../controllers/members.js'
 const router = Express.Router()
 
-router.get('/')
-router.post('/')
-router.get('/:id')
-router.put('/:id')
-router.delete('/:id')
+router.get('/', membersControlllers.getAll)
+router.post('/', membersControlllers.create)
+router.get('/:id', membersControlllers.getById)
+router.put('/:id', membersControlllers.update)
+router.delete('/:id', membersControlllers.remove)
 
 export default router
