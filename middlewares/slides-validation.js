767530bc81b3dfa816e8d3f41ceedb286b0da8b5
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-08 16:26:56 -0300

    Added validations for slides of organization

--- middlewares/slides-validation.js
+++ middlewares/slides-validation.js
@@ -1,2 +1,17 @@
 import { executeValidation } from './validation-index'
-import { check } from 'express-validator'
+import { check, param } from 'express-validator'
+
+const id = param('id', 'Ingresar el id')
+  .exists()
+  .isMongoId()
+const image = check('image', 'Ingresar una imagen')
+  .notEmpty()
+  .trim()
+const order = check('order', 'Ingresar la ubicacion de la imagen')
+  .isInt()
+
+const createValidation = [image, order, executeValidation]
+const removeValidation = [id, executeValidation]
+const updateValidation = [id, image, order, executeValidation]
+
+export { createValidation, removeValidation, updateValidation }
