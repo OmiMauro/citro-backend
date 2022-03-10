3b4cf9d1c044fa4eb67849f5861c51e9fbaa62a3
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-09 22:59:04 -0300

    Added methods for manage entity members

--- controllers/members.js
+++ controllers/members.js
@@ -1,9 +1,44 @@
 import membersService from '../services/members.js'
 
-const create = async (req, res) => { }
-const update = async (req, res) => {}
-const remove = async (req, res) => {}
-const getAll = async (req, res) => {}
-const getById = async (req, res) => {}
+const create = async (req, res, next) => {
+  try {
+    const member = await membersService.create(req.body)
+    res.status(201).json({ msg: 'El organizador fue creado.', data: member })
+  } catch (error) {
+    next(error)
+  }
+}
+const update = async (req, res, next) => {
+  try {
+    const member = await membersService.update(req.params.id, req.body)
+    res.status(201).json({ msg: 'El organizador fue actualizado.', data: member })
+  } catch (error) {
+    next(error)
+  }
+}
+const remove = async (req, res, next) => {
+  try {
+    const member = await membersService.remove(req.params.id)
+    res.status(200).json({ msg: 'El organizador fue eliminado.' })
+  } catch (error) {
+    next(error)
+  }
+}
+const getAll = async (req, res, next) => {
+  try {
+    const members = await membersService.getAll()
+    res.status(200).json({ data: members })
+  } catch (error) {
+    next(error)
+  }
+}
+const getById = async (req, res, next) => {
+  try {
+    const member = await membersService.getById(req.params.id)
+    res.status(200).json({ data: member })
+  } catch (error) {
+    next(error)
+  }
+}
 const membersControlllers = { create, update, remove, getAll, getById }
 export default membersControlllers
--- services/members.js
+++ services/members.js
@@ -1,19 +1,49 @@
 import membersRepository from '../repositories/members.js'
 
-const getById = async (id) => {
-  return await membersRepository.getById(id)
+const getById = async id => {
+  const member = await membersRepository.getById(id)
+  if (!member) {
+    const error = new Error('No se encontro un organizador con el ID')
+    error.status = 404
+    throw error
+  }
+  return member
 }
-const update = async (id, organization) => {
-  return await membersRepository.update(id, organization)
+const update = async (id, body) => {
+  const member = await membersRepository.update(id, body)
+  if (!member) {
+    const error = new Error('No se encontro un organizador con el ID')
+    error.status = 404
+    throw error
+  }
+  return member
 }
-const create = async (organization) => {
-  return await membersRepository.create(organization)
+const create = async (body) => {
+  const member = await membersRepository.create(body)
+  if (!member) {
+    const error = new Error('No se pudo crear al organizador')
+    error.status = 400
+    throw error
+  }
+  return member
 }
 const getAll = async () => {
-  return await membersRepository.getAll()
+  const members = await membersRepository.getAll()
+  if (!members) {
+    const error = new Error('No se encontraron todos los organizadores')
+    error.status = 400
+    throw error
+  }
+  return members
 }
 const remove = async id => {
-  return await membersRepository.remove(id)
+  const member = await membersRepository.remove(id)
+  if (!member) {
+    const error = new Error('No se encontro un organizador con el ID')
+    error.status = 404
+    throw error
+  }
+  return member
 }
 
 const membersService = { getById, update, create, getAll, remove }
