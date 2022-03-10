a11928942c5ae5becb832c40daf60a34ff8362a4
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-09 23:18:57 -0300

    Manage entity galeries of organization

--- services/galery.js
+++ services/galery.js
@@ -1,19 +1,49 @@
 import galeryRepository from '../repositories/galery.js'
 
 const getById = async (id) => {
-  return await galeryRepository.getById(id)
+  const galery = await galeryRepository.getById(id)
+  if (!galery) {
+    const error = new Error('No se pudo encontro el ID')
+    error.status = 404
+    throw error
+  }
+  return galery
 }
 const update = async (id, image) => {
-  return await galeryRepository.update(id, image)
+  const galery = await galeryRepository.update(id, image)
+  if (!galery) {
+    const error = new Error('No se pudo encontro el ID')
+    error.status = 404
+    throw error
+  }
+  return galery
 }
 const create = async (image) => {
-  return await galeryRepository.create(image)
+  const galery = await galeryRepository.create(image)
+  if (!galery) {
+    const error = new Error('No se pudo agregar la imagen')
+    error.status = 400
+    throw error
+  }
+  return galery
 }
 const getAll = async () => {
-  return await galeryRepository.getAll()
+  const galery = await galeryRepository.getAll()
+  if (!galery) {
+    const error = new Error('No se pudo encontrar imagenes')
+    error.status = 400
+    throw error
+  }
+  return galery
 }
 const remove = async id => {
-  return await galeryRepository.remove(id)
+  const galery = await galeryRepository.remove(id)
+  if (!galery) {
+    const error = new Error('No se pudo encontro el ID')
+    error.status = 404
+    throw error
+  }
+  return galery
 }
 const galeryServices = { getById, update, create, getAll, remove }
 
