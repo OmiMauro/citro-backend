f1f8998834123cf6a8cc7369e126ef80445d40b4
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-11 12:24:47 -0300

    Validations for organization

--- middlewares/organizations-validation.js
+++ middlewares/organizations-validation.js
@@ -1,37 +1,16 @@
-import { executeValidation } from './validation-index'
-import { check, param } from 'express-validator'
+import { executeValidation } from './validation-index.js'
+import { check } from 'express-validator'
+import { id, name, image, phone, email, urlFacebook, urlInstragram, urlWhatsapp } from './common-validation.js'
 
-const name = check('name', 'Ingresar el nombre')
-  .notEmpty()
-  .trim()
-const image = check('image', 'Ingresar la imagen o avatar')
-  .notEmpty()
-  .trim()
-const phone = check('phone', 'Ingresar el celular')
-  .notEmpty()
-  .trim()
-const email = check('email', 'Ingresar el email')
-  .notEmpty()
-  .trim()
-  .isEmail()
-const welcomeText = check('welcomeText', 'Ingresar un texto de bienvenida')
-  .trim()
-  .isString()
-const aboutUs = check('aboutUs', 'Ingresar una descripcion sobre la organizacion o grupo')
-  .trim()
-  .isString()
+const welcomeText = check('welcomeText')
+  .optional()
+  .isString().withMessage('Debe ingresar un texto de bienvenida')
 
-const urlFacebook = check('urlfacebook', 'Ingresar la URL del Facebook')
-  .trim()
-const urlInstragram = check('urlinstragram', 'Ingresar la URL del Instagram')
-  .trim()
-const urlWhatsapp = check('urlwhatsapp', 'Ingresar la URL del Whatsapp')
-  .trim()
-const id = param('id', 'Ingresar el ID como parametro')
-  .isMongoId()
+const aboutUs = check('aboutUs')
+  .optional()
+  .isString().withMessage('Debe ingresar un texto de bienvenida')
 
 const createValidation = [name, image, phone, email, welcomeText, aboutUs, urlFacebook, urlInstragram, urlWhatsapp, executeValidation]
-
 const removeValidation = [id, executeValidation]
 const updateValidation = [id, name, image, phone, email, welcomeText, aboutUs, urlFacebook, urlInstragram, urlWhatsapp, executeValidation]
 
