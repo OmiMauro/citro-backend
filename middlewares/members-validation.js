99e3014bb9d0ef38383ec649ccec0cae97de5c09
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-11 12:25:30 -0300

    Validations for members of organization

--- middlewares/members-validation.js
+++ middlewares/members-validation.js
@@ -1,31 +1,6 @@
-import { executeValidation } from './validation-index'
-import { check, param } from 'express-validator'
-
-const name = check('name', 'Ingresar el nombre')
-  .notEmpty()
-  .trim()
-
-const lastname = check('lastname', 'Ingresar el apellido')
-  .notEmpty()
-  .trim()
-
-const image = check('image', 'Ingresar la imagen o avatar')
-  .notEmpty()
-  .trim()
-const phone = check('phone', 'Ingresar el celular')
-  .notEmpty()
-  .trim()
-
-const urlFacebook = check('urlfacebook', 'Ingresar la URL del Facebook')
-  .trim()
-
-const urlInstragram = check('urlinstragram', 'Ingresar la URL del Instagram')
-  .trim()
-const urlWhatsapp = check('urlwhatsapp', 'Ingresar la URL del Whatsapp')
-  .trim()
-
-const id = param('id', 'Ingresar el ID como parametro')
-  .isMongoId()
+import { executeValidation } from './validation-index.js'
+// import { check, param } from 'express-validator'
+import { id, name, lastname, image, phone, urlInstragram, urlWhatsapp, urlFacebook } from './common-validation.js'
 
 const createValidation = [name, lastname, image, phone, urlFacebook, urlInstragram, urlWhatsapp, executeValidation]
 const removeValidation = [id, executeValidation]
