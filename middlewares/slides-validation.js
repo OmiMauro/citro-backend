34bb1989790876d4708098989bbfe980e11cc803
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-11 12:25:35 -0300

    Validations for slides

--- middlewares/slides-validation.js
+++ middlewares/slides-validation.js
@@ -1,13 +1,9 @@
 import { executeValidation } from './validation-index'
-import { check, param } from 'express-validator'
+import { check } from 'express-validator'
+import { id, image } from './common-validation.js'
 
-const id = param('id', 'Ingresar el id')
-  .exists()
-  .isMongoId()
-const image = check('image', 'Ingresar una imagen')
-  .notEmpty()
-  .trim()
-const order = check('order', 'Ingresar la ubicacion de la imagen')
+const order = check('order')
+  .exists().withMessage('Debe ingresar el orden del slide')
   .isInt()
 
 const createValidation = [image, order, executeValidation]
