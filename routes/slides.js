0b49b965fe0f6cc37bfb141d77c6fadf1b042cc0
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-12 12:25:49 -0300

    Added middlewares and persmises for all routes of organization

--- routes/slides.js
+++ routes/slides.js
@@ -2,12 +2,13 @@ import Express from 'express'
 import slidesControllers from '../controllers/slides.js'
 import filesMiddleware from '../middlewares/files.js'
 import { isAuth, isAdmin } from '../middlewares/auth.js'
+import { createValidation, idValidation, updateValidation } from '../middlewares/slides-validation.js'
 const router = Express.Router()
 
 router.get('/', slidesControllers.getAll)
-router.post('/', isAuth, isAdmin, filesMiddleware.validateSingleImage({ required: true }), slidesControllers.create)
-router.get('/:id', isAuth, isAdmin, slidesControllers.getById)
-router.put('/:id', isAuth, isAdmin, filesMiddleware.validateSingleImage(), slidesControllers.update)
-router.delete('/:id', isAuth, isAdmin, slidesControllers.remove)
+router.post('/', isAuth, isAdmin, filesMiddleware.validateSingleImage({ required: true }), createValidation, slidesControllers.create)
+router.get('/:id', isAuth, isAdmin, idValidation, slidesControllers.getById)
+router.put('/:id', isAuth, isAdmin, filesMiddleware.validateSingleImage(), updateValidation, slidesControllers.update)
+router.delete('/:id', isAuth, isAdmin, idValidation, slidesControllers.remove)
 
 export default router
