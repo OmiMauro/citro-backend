4e6da5b907bf0139c44d92289da28c33b0738506
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-12 12:26:19 -0300

    Added middlewares and persmises for all routes of organization

--- routes/organization.js
+++ routes/organization.js
@@ -2,11 +2,11 @@ import Express from 'express'
 import organizationControllers from '../controllers/organization.js'
 import filesMiddleware from '../middlewares/files.js'
 import { isAuth, isAdmin } from '../middlewares/auth.js'
-
+import { createValidation, updateValidation } from '../middlewares/organizations-validation.js'
 const router = Express.Router()
 
-router.post('/', isAuth, isAdmin, filesMiddleware.validateSingleImage({ required: true }), organizationControllers.create)
+router.post('/', isAuth, isAdmin, filesMiddleware.validateSingleImage({ required: true }), createValidation, organizationControllers.create)
 router.get('/', organizationControllers.getById)
-router.put('/:id', isAuth, isAdmin, filesMiddleware.validateSingleImage(), organizationControllers.update)
+router.put('/:id', isAuth, isAdmin, filesMiddleware.validateSingleImage(), updateValidation, organizationControllers.update)
 
 export default router
