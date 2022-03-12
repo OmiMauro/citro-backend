4c455c50ff636a5d145bd051537bd5418e3682c9
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-12 11:10:55 -0300

    Fixes into images

--- repositories/images.js
+++ repositories/images.js
@@ -1,18 +1,18 @@
-import { Images } from '../models/images.js'
+import { Image } from '../models/images.js'
 
 const getById = async (id) => {
-  return await Images.findById(id)
+  return await Image.findById(id)
 }
 const update = async (id, image) => {
-  return await Images.findByIdAndUpdate(id, image)
+  return await Image.findByIdAndUpdate(id, image)
 }
 const create = async (image) => {
-  return await Images.create(image)
+  return await Image.create(image)
 }
 const getAll = async () => {
-  return await Images.find({})
+  return await Image.find({})
 }
 const remove = async (id) => {
-  return await Images.findByIdAndRemove(id)
+  return await Image.findByIdAndRemove(id)
 }
 export default { getById, update, create, getAll, remove }
