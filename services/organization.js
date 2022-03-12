4eae5e2c20505ecdcc087735f87d43e8cdb70120
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-12 11:14:17 -0300

    Added image to organization

--- services/organization.js
+++ services/organization.js
@@ -1,4 +1,6 @@
 import organizationRepository from '../repositories/organizations.js'
+import filesModule from '../modules/files.js'
+import imagesRepository from '../repositories/images.js'
 
 const getById = async (id) => {
   const organization = await organizationRepository.getById(id)
@@ -11,7 +13,6 @@ const getById = async (id) => {
 }
 const update = async (id, body) => {
   const organization = await organizationRepository.update(id, body)
-  console.log(organization)
   if (!organization) {
     const error = new Error('No se pudo encontrar la organizacion con el ID')
     error.status = 404
@@ -19,7 +20,10 @@ const update = async (id, body) => {
   }
   return organization
 }
-const create = async (body) => {
+const create = async (body, imageFile) => {
+  const imageUpload = await filesModule.uploadFile(imageFile, 'organization')
+  const image = await imagesRepository.create(imageUpload)
+  body.image_id = image.id
   const organization = await organizationRepository.create(body)
   if (!organization) {
     const error = new Error('No se puede crear la organizacion')
