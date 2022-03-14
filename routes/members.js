7033c6815b3b4d548efe83ca98041d65cca288b4
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-14 16:23:20 -0300

    Add validations of data in body for all routes of members

--- routes/members.js
+++ routes/members.js
@@ -2,12 +2,13 @@ import Express from 'express'
 import membersControlllers from '../controllers/members.js'
 import filesMiddleware from '../middlewares/files.js'
 import { isAuth, isAdmin } from '../middlewares/auth.js'
+import { createValidation, updateValidation, idValidation } from '../middlewares/members-validation.js'
 const router = Express.Router()
 
 router.get('/', membersControlllers.getAll)
-router.post('/', isAuth, isAdmin, filesMiddleware.validateSingleImage({ required: true }), membersControlllers.create)
+router.post('/', isAuth, isAdmin, filesMiddleware.validateSingleImage({ required: true }), createValidation, membersControlllers.create)
 router.get('/:id', isAuth, isAdmin, membersControlllers.getById)
-router.put('/:id', isAuth, isAdmin, filesMiddleware.validateSingleImage(), membersControlllers.update)
-router.delete('/:id', isAuth, isAdmin, membersControlllers.remove)
+router.put('/:id', isAuth, isAdmin, filesMiddleware.validateSingleImage(), updateValidation, membersControlllers.update)
+router.delete('/:id', isAuth, isAdmin, idValidation, membersControlllers.remove)
 
 export default router
