4dbd7c7e19bf7e10057245bb34e2ab8ec86e3881
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-14 16:21:25 -0300

    Add validation data for all routes of slides

--- middlewares/galery-validation.js
+++ middlewares/galery-validation.js
@@ -1,9 +1,7 @@
 import { executeValidation } from './validation-index.js'
-import { check, param } from 'express-validator'
 import { id, image } from './common-validation.js'
 
 const createValidation = [image, executeValidation]
-const removeValidation = [id, executeValidation]
-const updateValidation = [id, image, executeValidation]
+const idValidation = [id, executeValidation]
 
-export { createValidation, removeValidation, updateValidation }
+export { createValidation, idValidation }
--- routes/galery.js
+++ routes/galery.js
@@ -2,12 +2,13 @@ import Express from 'express'
 import galeryControllers from '../controllers/galery.js'
 import filesMiddleware from '../middlewares/files.js'
 import { isAuth, isAdmin } from '../middlewares/auth.js'
+import { createValidation, idValidation } from '../middlewares/galery-validation.js'
 const router = Express.Router()
 
 router.get('/', galeryControllers.getAll)
-router.post('/', isAuth, isAdmin, filesMiddleware.validateArrayImage({ required: true }), galeryControllers.create)
-router.get('/:id', isAuth, isAdmin, galeryControllers.getById)
+router.post('/', isAuth, isAdmin, filesMiddleware.validateArrayImage({ required: true }), createValidation, galeryControllers.create)
+router.get('/:id', isAuth, isAdmin, idValidation, galeryControllers.getById)
 router.put('/:id', isAuth, isAdmin, filesMiddleware.validateSingleImage(), galeryControllers.update)
-router.delete('/:id', isAuth, isAdmin, galeryControllers.remove)
+router.delete('/:id', isAuth, isAdmin, idValidation, galeryControllers.remove)
 
 export default router
