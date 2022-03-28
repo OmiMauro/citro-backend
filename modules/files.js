654347f77f1f1b73014cd29056f540174ba5ab29
Manu Ominuka <ominuka.mauro@gmail.com>
2022-03-28 17:19:33 -0300

    Folder to citros

--- modules/files.js
+++ modules/files.js
@@ -3,26 +3,30 @@ import path from 'path'
 import fs from 'fs'
 
 const uploadFile = async (file, deleteTempFile = true, folder) => {
-  const fileToCreate = {
-    path: file.path,
-    ext: path.extname(file.originalname),
-    contentType: file.mimetype
-  }
-  try {
-    const fileUploadedToCloud = await cloudinary.uploadFile(fileToCreate, folder)
-    if (deleteTempFile) {
-      try {
-        await deleteLocalFile(file.path)
-      } catch (error) {
-        console.error('error to remove file', error)
-      }
-    }
-    return fileUploadedToCloud
-  } catch (error) {
-    console.error('error to remove file', error)
-  }
+	const fileToCreate = {
+		path: file.path,
+		ext: path.extname(file.originalname),
+		contentType: file.mimetype
+	}
+	try {
+		folder = `/citros/${folder}`
+		const fileUploadedToCloud = await cloudinary.uploadFile(
+			fileToCreate,
+			folder
+		)
+		if (deleteTempFile) {
+			try {
+				await deleteLocalFile(file.path)
+			} catch (error) {
+				console.error('error to remove file', error)
+			}
+		}
+		return fileUploadedToCloud
+	} catch (error) {
+		console.error('error to remove file', error)
+	}
 }
 
-const deleteLocalFile = async filePath => await fs.promises.unlink(filePath)
+const deleteLocalFile = async (filePath) => await fs.promises.unlink(filePath)
 
 export default { uploadFile, deleteLocalFile }
