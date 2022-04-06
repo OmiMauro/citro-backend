65166bb34b0d02d058b6717d6851caeeee85117b
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-05 21:30:47 -0300

    Refactor method for register a user. Add folder to cloudinary storage picture

--- services/auth.js
+++ services/auth.js
@@ -1,4 +1,3 @@
-
 import rolesRepository from '../repositories/roles.js'
 import usersRepository from '../repositories/users.js'
 import bcrypt from 'bcrypt'
@@ -8,66 +7,70 @@ import imagesRepository from '../repositories/images.js'
 import { createToken } from '../modules/auth.js'
 
 const register = async (body, imageFile) => {
-  const isEmailExists = await usersRepository.getByEmail(body.email)
-  if (isEmailExists) {
-    filesModule.deleteLocalFile(imageFile)
-    const error = new Error('El email ya se encuentra registrado')
-    error.status = 400
-    throw error
-  }
-  const imageUpload = await filesModule.uploadFile(imageFile, 'slides')
-  const image = await imagesRepository.create(imageUpload)
-  body.image_id = image.id
-  // where 1 is ID of role Standard, Admin is 2
-  // This is harcoded because only database is possible assign a admin
-  const standarRole = await rolesRepository.getById(1)
-  const newUser = {
-    ...body,
-    roledId: standarRole.id,
-    password: await bcrypt.hashSync(body.password, 11)
-  }
-  const user = await usersRepository.create(newUser)
-  if (!user) {
-    const error = new Error('No se puede crear el usuario')
-    error.status = 400
-    throw error
-  }
-  // never return a password, therefore is hashed
-  user.password = ''
-  return user
+	const emailExists = await isEmailExists(body.email)
+	if (emailExists) {
+		await removeFile(imageFile)
+		const error = new Error('El email ya se encuentra registrado')
+		error.status = 400
+		throw error
+	}
+	const imageUpload = await filesModule.uploadFile(imageFile, true, 'users')
+	const image = await imagesRepository.create(imageUpload)
+	body.image_id = image.id
+	// where 1 is ID of role Standard, Admin is 2
+	// This is harcoded because only database is possible assign a admin
+	const standarRole = await rolesRepository.getById(1)
+	const newUser = {
+		...body,
+		roleId: standarRole._id,
+		password: await bcrypt.hashSync(body.password, 11)
+	}
+	const user = await usersRepository.create(newUser)
+	if (!user) {
+		const error = new Error('No se puede crear el usuario')
+		error.status = 400
+		throw error
+	}
+	user.password = ''
+	return user
 }
-const login = async body => {
-  const user = await usersRepository.getByEmail(body.email)
-  if (!user) {
-    const error = new Error('Email o contraseñas incorrectas')
-    error.status = 404
-    throw error
-  }
-  const comparePassword = await bcrypt.compareSync(body.password, user.password)
-  if (!comparePassword) {
-    const error = new Error('Email o contraseñas incorrectas')
-    error.status = 404
-    throw error
-  }
-  const payload = {
-    email: user.email,
-    userId: user._id,
-    roleId: user.roleId,
-    name: user.name,
-    lastname: user.lastname
-  }
-  const token = createToken(payload)
-  return token
+const login = async (body) => {
+	const user = await usersRepository.getByEmail(body.email)
+	if (!user) {
+		const error = new Error('Email o contraseñas incorrectas')
+		error.status = 404
+		throw error
+	}
+	const comparePassword = await bcrypt.compareSync(body.password, user.password)
+	if (!comparePassword) {
+		const error = new Error('Email o contraseñas incorrectas')
+		error.status = 400
+		throw error
+	}
+	const payload = {
+		email: user.email,
+		userId: user._id,
+		roleId: user.roleId,
+		name: user.name,
+		lastname: user.lastname
+	}
+	const token = createToken(payload)
+	return token
 }
 
 const getAll = async () => {
-  const users = await usersRepository.getAll()
-  if (!users) {
-    const error = new Error('No se puede encontrar los usuarios')
-    error.status = 404
-    throw error
-  }
-  return users
+	const users = await usersRepository.getAll()
+	if (!users) {
+		const error = new Error('No se puede encontrar los usuarios')
+		error.status = 404
+		throw error
+	}
+	return users
 }
+
+const isEmailExists = async (email) => await usersRepository.getByEmail(email)
+
+const removeFile = async (filePath) =>
+	await filesModule.deleteLocalFile(filePath)
 
 export default { register, login, getAll }
