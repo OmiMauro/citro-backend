343fcfcfd1360bdb7ff23f8a1d27988da440c533
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-05 21:20:32 -0300

    Added param to folder in cloud

--- services/organization.js
+++ services/organization.js
@@ -3,34 +3,38 @@ import filesModule from '../modules/files.js'
 import imagesRepository from '../repositories/images.js'
 
 const getById = async (id) => {
-  const organization = await organizationRepository.getById(id)
-  if (!organization) {
-    const error = new Error('No se pudo encontrar la organizacion con el ID')
-    error.status = 404
-    throw error
-  }
-  return organization
+	const organization = await organizationRepository.getById(id)
+	if (!organization) {
+		const error = new Error('No se pudo encontrar la organizacion con el ID')
+		error.status = 404
+		throw error
+	}
+	return organization
 }
 const update = async (id, body) => {
-  const organization = await organizationRepository.update(id, body)
-  if (!organization) {
-    const error = new Error('No se pudo encontrar la organizacion con el ID')
-    error.status = 404
-    throw error
-  }
-  return organization
+	const organization = await organizationRepository.update(id, body)
+	if (!organization) {
+		const error = new Error('No se pudo encontrar la organizacion con el ID')
+		error.status = 404
+		throw error
+	}
+	return organization
 }
 const create = async (body, imageFile) => {
-  const imageUpload = await filesModule.uploadFile(imageFile, 'organization')
-  const image = await imagesRepository.create(imageUpload)
-  body.image_id = image.id
-  const organization = await organizationRepository.create(body)
-  if (!organization) {
-    const error = new Error('No se puede crear la organizacion')
-    error.status = 400
-    throw error
-  }
-  return organization
+	const imageUpload = await filesModule.uploadFile(
+		imageFile,
+		true,
+		'organization'
+	)
+	const image = await imagesRepository.create(imageUpload)
+	body.image_id = image.id
+	const organization = await organizationRepository.create(body)
+	if (!organization) {
+		const error = new Error('No se puede crear la organizacion')
+		error.status = 400
+		throw error
+	}
+	return organization
 }
 
 export default { getById, update, create }
