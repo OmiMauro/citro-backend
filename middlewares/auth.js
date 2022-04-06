f030adb15dd702c8f71333e60ff3c82c4ce5d6ab
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-05 21:21:34 -0300

    Set to undefined pwd when the user was logged

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,45 +5,49 @@ import usersRepository from '../repositories/users.js'
 const { adminRoleName } = config
 
 const isAuth = async (req, res, next) => {
-  try {
-    const token = getTokenPayload(req)
-    const user = await usersRepository.getById(token.userId)
-    if (!user) {
-      const error = new Error('Token no valido')
-      error.status = 403
-      throw error
-    }
-    req.authUser = user
-    next()
-  } catch (error) {
-    next(error)
-  }
+	try {
+		const token = getTokenPayload(req)
+		const user = await usersRepository.getById(token.userId)
+		if (!user) {
+			const error = new Error('Token no valido')
+			error.status = 403
+			throw error
+		}
+		user.password = ''
+		req.authUser = user
+		next()
+	} catch (error) {
+		next(error)
+	}
 }
 const isAdmin = async (req, res, next) => {
-  try {
-    const { authUser } = req
-    const roleUser = await rolesRepository.getById(authUser.roleId)
-    if (roleUser.name !== adminRoleName) {
-      const error = new Error('Recurso solo para administradores')
-      error.status = 403
-      throw error
-    }
-    next()
-  } catch (error) {
-    next(error)
-  }
+	try {
+		const { authUser } = req
+		const roleUser = await rolesRepository.getById(authUser.roleId)
+		if (roleUser.name !== adminRoleName) {
+			const error = new Error('Recurso solo para administradores')
+			error.status = 403
+			throw error
+		}
+		next()
+	} catch (error) {
+		next(error)
+	}
 }
-const isOwnUser = () => { }
+const isOwnUser = () => {}
 
 const getTokenPayload = (req) => {
-  const tokenHeader = req.headers.authorization
-  const token = tokenHeader && tokenHeader.startsWith('Bearer ') && tokenHeader.split(' ')[1]
-  if (!token) {
-    const error = new Error('Por favor, ingrese un token en el headers')
-    error.status = 403
-    throw error
-  }
-  return verifyToken(token)
+	const tokenHeader = req.headers.authorization
+	const token =
+		tokenHeader &&
+		tokenHeader.startsWith('Bearer ') &&
+		tokenHeader.split(' ')[1]
+	if (!token) {
+		const error = new Error('Por favor, ingrese un token en el headers')
+		error.status = 403
+		throw error
+	}
+	return verifyToken(token)
 }
 
 export { isAdmin, isAuth, isOwnUser, getTokenPayload }
