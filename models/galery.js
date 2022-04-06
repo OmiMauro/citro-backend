20da344dddf932e628446da9368466fafab52fae
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-05 21:24:14 -0300

    fixes

--- models/galery.js
+++ models/galery.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
-const galerySchema = new Schema({
-  image_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Images'
-  }
-
-}, {
-  timestamps: true
-})
+const galerySchema = new Schema(
+	{
+		image_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Image'
+		}
+	},
+	{
+		timestamps: true
+	}
+)
 
 const Galery = mongoose.model('Galery', galerySchema)
 
