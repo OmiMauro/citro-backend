17ccb261ce73018d2c20e0e2df3e4143cc7b3d3a
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-05 21:24:34 -0300

    Added populate for getall images of galery

--- repositories/galery.js
+++ repositories/galery.js
@@ -1,18 +1,18 @@
 import { Galery } from '../models/galery.js'
 
 const getById = async (id) => {
-  return await Galery.findById(id)
+	return await Galery.findById(id)
 }
 const update = async (id, image) => {
-  return await Galery.findByIdAndUpdate(id, image)
+	return await Galery.findByIdAndUpdate(id, image)
 }
 const create = async (imageId) => {
-  return await Galery.create({ image_id: imageId })
+	return await Galery.create({ image_id: imageId })
 }
 const getAll = async () => {
-  return await Galery.find({})
+	await Galery.find({}).populate('image_id')
 }
 const remove = async (id) => {
-  return await Galery.findByIdAndRemove(id)
+	return await Galery.findByIdAndRemove(id)
 }
 export default { getById, update, create, getAll, remove }
