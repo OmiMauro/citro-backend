f0b8f428e6aadf7ff9bed368f91255c2f122c2d0
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-13 11:26:28 -0300

    fixes: Remove destructuring of token from configs

--- modules/auth.js
+++ modules/auth.js
@@ -1,18 +1,19 @@
 import jwt from 'jsonwebtoken'
 import config from '../config/config.js'
 
-const { token } = config
-const createToken = payload => {
-  return jwt.sign(payload, token.secret, { expiresIn: token.expiresIn })
+const createToken = (payload) => {
+	return jwt.sign(payload, config.token.secret, {
+		expiresIn: config.token.expiresIn
+	})
 }
 
-const verifyToken = token => {
-  try {
-    return jwt.verify(token, token.secret)
-  } catch (error) {
-    error.message = 'Token Bearer invalid'
-    error.status = 401
-    throw error
-  }
+const verifyToken = (token) => {
+	try {
+		return jwt.verify(token, config.token.secret)
+	} catch (error) {
+		error.message = 'Token Bearer invalid'
+		error.status = 401
+		throw error
+	}
 }
 export { createToken, verifyToken }
