56183307b0b01c6d3ee60e59f207a7411cdc8807
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-13 11:25:22 -0300

    Added width and heigth of image

--- models/images.js
+++ models/images.js
@@ -1,22 +1,16 @@
 import mongoose from 'mongoose'
 
 const imageSchema = new mongoose.Schema({
-  asset_id:
-   { type: String },
-  public_id:
-   { type: String },
-  version_id:
-   { type: String },
-  signature:
-   { type: String },
-  url:
-   { type: String },
-  secure_url:
-   { type: String },
-  original_filename:
-   { type: String },
-  api_key:
-   { type: String }
+	asset_id: { type: String },
+	public_id: { type: String },
+	version_id: { type: String },
+	signature: { type: String },
+	url: { type: String },
+	secure_url: { type: String },
+	original_filename: { type: String },
+	api_key: { type: String },
+	height: { type: Number },
+	width: { type: Number }
 })
 
 const Image = mongoose.model('Image', imageSchema)
--- services/galery.js
+++ services/galery.js
@@ -22,12 +22,12 @@ const update = async (id, image) => {
 }
 const create = async (body, files) => {
 	const allPics = []
-	files.forEach(async (item) => {
+	await files.forEach(async (item) => {
 		const imageUpload = await filesModule.uploadFile(item, true, 'galery')
 		const image = await imagesRepository.create(imageUpload)
 		const galery = await galeryRepository.create(image._id)
 		if (!galery || !image || !imageUpload) {
-			filesModule.deleteLocalFile(item)
+			await filesModule.deleteLocalFile(item)
 			const error = new Error('No se pudo agregar la imagen')
 			error.status = 400
 			throw error
@@ -47,6 +47,7 @@ const getAll = async () => {
 	return galery
 }
 const remove = async (id) => {
+	//missing remove image from cloudinary
 	const galery = await galeryRepository.remove(id)
 	if (!galery) {
 		const error = new Error('No se pudo encontrar el ID')
