8e1df10fe15ee9c66aa19485bc4ff048be8fb53d
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-24 18:20:08 -0300

    Added logger errors 500 in the service of Sentry

--- config/config.js
+++ config/config.js
@@ -36,5 +36,8 @@ export default {
 		cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 		api_key: process.env.CLOUDINARY_API_KEY,
 		api_secret: process.env.CLOUDINARY_API_SECRET
+	},
+	sentry: {
+		dsn: process.env.SENTRY_DSN
 	}
 }
--- server.js
+++ server.js
@@ -6,21 +6,48 @@ import 'dotenv/config'
 import createError from 'http-errors'
 import connectDB from './models/index.js'
 import { indexRouter } from './routes/index.js'
-
+import Sentry from '@sentry/node'
+import Tracing from '@sentry/tracing'
+import config from './config/config.js'
 const app = express()
 
 const corsOptions = {
 	origin: '*', // Reemplazar con dominio
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+
+// Configs for Sentry
+Sentry.init({
+	dsn: config.sentry.dsn,
+	integrations: [
+		// enable HTTP calls tracing
+		new Sentry.Integrations.Http({ tracing: true }),
+		// enable Express.js middleware tracing
+		new Tracing.Integrations.Express({ app })
+	],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0
+})
+// RequestHandler creates a separate execution context using domains, so that every
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+
 app.use(cors(corsOptions))
 
 // middleware
 app.use(express.json())
 app.use(cookieParser())
 app.use(logger('dev'))
+
 connectDB()
+
 app.use('/api/v2', indexRouter)
+
+app.use(Sentry.Handlers.errorHandler())
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
