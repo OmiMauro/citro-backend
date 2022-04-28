49001a207ce50d98f8fabeb0a2169af304947173
Manu Ominuka <ominuka.mauro@gmail.com>
2022-04-28 09:55:22 -0300

    Refactor response when catch error in request

--- controllers/galery.js
+++ controllers/galery.js
@@ -5,7 +5,9 @@ const create = async (req, res, next) => {
 		const galery = await galeryServices.create(req.body, req.files)
 		return res.status(201).json({ msg: 'La imagen fue creada.', data: galery })
 	} catch (error) {
-		next(error)
+		res
+			.status(error.status)
+			.json({ errors: [{ msg: error.message }], data: false })
 	}
 }
 const update = async (req, res, next) => {
@@ -13,7 +15,9 @@ const update = async (req, res, next) => {
 		const galery = await galeryServices.update(req.params.id, req.body)
 		res.status(201).json({ msg: 'La imagen fue actualizada', data: galery })
 	} catch (error) {
-		next(error)
+		res
+			.status(error.status)
+			.json({ errors: [{ msg: error.message }], data: false })
 	}
 }
 const remove = async (req, res, next) => {
@@ -21,7 +25,9 @@ const remove = async (req, res, next) => {
 		const galery = await galeryServices.remove(req.body)
 		res.status(200).json({ msg: 'La imagen se elimino.', data: galery })
 	} catch (error) {
-		next(error)
+		res
+			.status(error.status)
+			.json({ errors: [{ msg: error.message }], data: false })
 	}
 }
 const getAll = async (req, res, next) => {
@@ -29,7 +35,9 @@ const getAll = async (req, res, next) => {
 		const galeries = await galeryServices.getAll()
 		res.status(201).json({ data: galeries })
 	} catch (error) {
-		next(error)
+		res
+			.status(error.status)
+			.json({ errors: [{ msg: error.message }], data: false })
 	}
 }
 const getById = async (req, res, next) => {
@@ -37,7 +45,9 @@ const getById = async (req, res, next) => {
 		const galery = await galeryServices.getById(req.params.id)
 		res.status(201).json({ data: galery })
 	} catch (error) {
-		next(error)
+		res
+			.status(error.status)
+			.json({ errors: [{ msg: error.message }], data: false })
 	}
 }
 
