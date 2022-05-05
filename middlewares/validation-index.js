3025a0da79f6ab0601de868c914da92ab1b6563e
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-05 14:59:21 -0300

    Remove files in folder public/temp when happen errors in validation data from express-validator

--- middlewares/validation-index.js
+++ middlewares/validation-index.js
@@ -1,11 +1,12 @@
 import { validationResult } from 'express-validator'
-
-const executeValidation = (req, res, next) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  }
-  next()
+import filesModule from '../modules/files.js'
+const executeValidation = async (req, res, next) => {
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		if (req.file) await filesModule.deleteLocalFile(req.file)
+		return res.status(400).json({ errors: errors.array() })
+	}
+	next()
 }
 
 export { executeValidation }
