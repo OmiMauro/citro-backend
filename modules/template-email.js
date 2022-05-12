9ab3c023b06ed756a66965e976d658c9a7a98b4a
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-12 11:34:31 -0300

    Add method for create template that receive data and path

---
+++ modules/template-email.js
@@ -0,0 +1,10 @@
+import ejs from 'ejs'
+import path from 'path'
+
+export const createTemplate = async (data, nameFile) => {
+	const response = ejs.renderFile(
+		path.join(process.cwd(), 'templates', nameFile),
+		data
+	)
+	return response
+}
