8a2cb537f076667f976f9fcadf36817cd2b4e3cf
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-20 23:35:11 -0300

    Change name imported

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 import Express from 'express'
 import usersController from '../controllers/users.js'
-import { updateValidation, idValidation } from '../middlewares/auth-validation.js'
+import authValidation from '../middlewares/auth-validation.js'
 
 const router = Express.Router()
 
-router.put('/:id', updateValidation, usersController.update)
-router.delete('/:id', idValidation, usersController.remove)
+router.put('/:id', authValidation.updateValidation, usersController.update)
+router.delete('/:id', authValidation.idValidation, usersController.remove)
 
 export default router
