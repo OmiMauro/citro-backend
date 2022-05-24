d64d4431ba9fdc2ce9da9ca5001fe49a4a2c238b
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-24 10:56:37 -0300

    Populate getById organizataion

--- repositories/organizations.js
+++ repositories/organizations.js
@@ -1,13 +1,16 @@
 import { Organizations } from '../models/organization.js'
 
 const getById = async (id) => {
-  return await Organizations.findById(id)
+	return await Organizations.findById(id).populate(
+		'image_id',
+		'public_id url secure_url height width'
+	)
 }
 const update = async (id, organization) => {
-  return await Organizations.findByIdAndUpdate(id, organization)
+	return await Organizations.findByIdAndUpdate(id, organization)
 }
 const create = async (organization) => {
-  return await Organizations.create(organization)
+	return await Organizations.create(organization)
 }
 
 export default { getById, update, create }
