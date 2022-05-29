c76c203bcda4b199405fdc3414c897b376732d5d
Manu Ominuka <ominuka.mauro@gmail.com>
2022-05-29 11:49:27 -0300

    Add middlewares for isAuth and IsOwnUser

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,30 @@
 import Express from 'express'
 import usersController from '../controllers/users.js'
 import authValidation from '../middlewares/auth-validation.js'
+import { isOwnUser, isAdmin, isAuth } from '../middlewares/auth.js'
 
 const router = Express.Router()
-
-router.put('/:id', authValidation.updateValidation, usersController.update)
-router.delete('/:id', authValidation.idValidation, usersController.remove)
+router.get(
+	'/:id',
+	isAuth,
+	isOwnUser,
+	authValidation.idValidation,
+	usersController.getById
+)
+router.put(
+	'/:id',
+	isAuth,
+	isOwnUser,
+	authValidation.updateValidation,
+	usersController.update
+)
+router.delete(
+	'/:id',
+	isAuth,
+	isAdmin,
+	isOwnUser,
+	authValidation.idValidation,
+	usersController.remove
+)
 
 export default router
