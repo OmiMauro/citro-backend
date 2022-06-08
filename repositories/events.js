97cd6ea019c21b9b64b6370beccd490b64e2eb95
Manu <ominuka.mauro@gmail.com>
2022-06-08 11:43:43 -0300

    Add service for update a subdocument for events

--- repositories/events.js
+++ repositories/events.js
@@ -1,18 +1,27 @@
 import Events from '../models/events.js'
 
 const getById = async (id) => {
-	return await Events.findById(id)
+  return await Events.findById(id)
 }
 const update = async (id, event) => {
-	return await Events.findByIdAndUpdate(id, event)
+  return await Events.findByIdAndUpdate(id, event)
 }
+
+const updateSubdocument = async ({ _id, subDocID }, field, body) => {
+  return await Events.findOneAndUpdate(
+    { _id: id, 'field._id': subDocID },
+    { 'field.$': body },
+    { new: true }
+  )
+}
+
 const create = async (event) => {
-	return await Events.create(event)
+  return await Events.create(event)
 }
 const getAll = async () => {
-	return await Events.find({})
+  return await Events.find({}).populate('image_id')
 }
 const remove = async (id) => {
-	return await Events.findByIdAndRemove(id)
+  return await Events.findByIdAndRemove(id)
 }
-export default { getById, update, create, getAll, remove }
+export default { getById, update, create, getAll, remove, updateSubdocument }
