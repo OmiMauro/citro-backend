9c53f6113a668ebba20e09db8514277affbb74c6
Manu <ominuka.mauro@gmail.com>
2022-06-08 11:43:08 -0300

    Feat: update hotel by Id for a event

--- controllers/events.js
+++ controllers/events.js
@@ -1,74 +1,94 @@
 import eventsServices from '../services/events.js'
 
 const create = async (req, res, next) => {
-	try {
-		const event = await eventsServices.create(req.body, req.file)
-		return res.status(201).json({ msg: 'El evento fue creado.', data: event })
-	} catch (error) {
-		res
-			.status(error.status)
-			.json({ errors: [{ msg: error.message }], data: false })
-	}
+  try {
+    const event = await eventsServices.create(req.body, req.file)
+    return res.status(201).json({ msg: 'El evento fue creado.', data: event })
+  } catch (error) {
+    res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: false })
+  }
 }
 const update = async (req, res, next) => {
-	try {
-		const event = await eventsServices.update(req.params.id, req.body)
-		return res
-			.status(201)
-			.json({ msg: 'El evento fue actualizado.', data: event })
-	} catch (error) {
-		return res
-			.status(error.status)
-			.json({ errors: [{ msg: error.message }], data: false })
-	}
+  try {
+    const event = await eventsServices.update(req.params.id, req.body)
+    return res
+      .status(201)
+      .json({ msg: 'El evento fue actualizado.', data: event })
+  } catch (error) {
+    return res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: false })
+  }
 }
 const remove = async (req, res, next) => {
-	try {
-		const event = await eventsServices.remove(req.params.id)
-		return res.status(200).json({
-			data: { _id: event._id },
-			msg: 'Se elimino con exito el evento'
-		})
-	} catch (error) {
-		return res
-			.status(error.status)
-			.json({ errors: [{ msg: error.message }], data: false })
-	}
+  try {
+    const event = await eventsServices.remove(req.params.id)
+    return res.status(200).json({
+      data: { _id: event._id },
+      msg: 'Se elimino con exito el evento',
+    })
+  } catch (error) {
+    return res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: false })
+  }
 }
 const getAll = async (req, res, next) => {
-	try {
-		const events = await eventsServices.getAll()
-		return res.status(200).json({
-			data: events
-		})
-	} catch (error) {
-		console.log(error)
-		return res
-			.status(error.status)
-			.json({ errors: [{ msg: error.message }], data: false })
-	}
+  try {
+    const events = await eventsServices.getAll()
+    return res.status(200).json({
+      data: events,
+    })
+  } catch (error) {
+    console.log(error)
+    return res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: false })
+  }
 }
 const getById = async (req, res, next) => {
-	try {
-		const event = await eventsServices.getById(req.params.id)
-		return res.status(200).json({ data: event })
-	} catch (error) {
-		return res
-			.status(error.status)
-			.json({ errors: [{ msg: error.message }], data: false })
-	}
+  try {
+    const event = await eventsServices.getById(req.params.id)
+    return res.status(200).json({ data: event })
+  } catch (error) {
+    return res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: false })
+  }
 }
 const updateImage = async (req, res, next) => {
-	try {
-		const event = await eventsServices.updateImage(req.params.id, req.file)
-		return res
-			.status(200)
-			.json({ data: event, msg: 'Se actualizo la imagen con exito' })
-	} catch (error) {
-		return res
-			.status(error.status)
-			.json({ errors: [{ msg: error.message }], data: false })
-	}
+  try {
+    const event = await eventsServices.updateImage(req.params.id, req.file)
+    return res
+      .status(200)
+      .json({ data: event, msg: 'Se actualizo la imagen con exito' })
+  } catch (error) {
+    return res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: false })
+  }
 }
 
-export default { create, update, remove, getAll, getById, updateImage }
+const updateHotelById = async (req, res, next) => {
+  try {
+    const hotel = await eventsServices.updateHotelById(req.params, req.body)
+    return res
+      .status(200)
+      .json({ data: hotel, msg: 'Se actualizo el hotel con éxito' })
+  } catch (error) {
+    return res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: false })
+  }
+}
+export default {
+  create,
+  update,
+  remove,
+  getAll,
+  getById,
+  updateImage,
+  updateHotelById,
+}
--- routes/events.js
+++ routes/events.js
@@ -3,31 +3,37 @@ import eventsController from '../controllers/events.js'
 import { isAdmin, isAuth } from '../middlewares/auth.js'
 import filesMiddleware from '../middlewares/files.js'
 import {
-	idValidation,
-	createValidation,
-	updateValidation
+  idValidation,
+  createValidation,
+  updateValidation,
 } from '../middlewares/events-validation.js'
 const router = Express.Router()
 
 router.get('/', eventsController.getAll)
 router.post(
-	'/',
-	filesMiddleware.validateSingleImage({ required: true }),
-	createValidation,
-	isAuth,
-	isAdmin,
-	eventsController.create
+  '/',
+  filesMiddleware.validateSingleImage({ required: true }),
+  createValidation,
+  isAuth,
+  isAdmin,
+  eventsController.create
 )
 router.get('/:id', idValidation, eventsController.getById)
-router.put('/:id', updateValidation, isAuth, isAdmin, eventsController.update)
+router.put('/:id', isAuth, isAdmin, updateValidation, eventsController.update)
+router.patch(
+  '/:id/hotel/:hotelId',
+  isAuth,
+  isAdmin,
+  eventsController.updateHotelById
+)
 
 router.patch(
-	'/:id/image',
-	isAuth,
-	isAdmin,
-	idValidation,
-	filesMiddleware.validateSingleImage({ required: true }),
-	eventsController.updateImage
+  '/:id/image',
+  isAuth,
+  isAdmin,
+  idValidation,
+  filesMiddleware.validateSingleImage({ required: true }),
+  eventsController.updateImage
 )
 router.delete('/:id', idValidation, isAuth, isAdmin, eventsController.remove)
 
