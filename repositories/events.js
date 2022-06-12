e01df88b44928400fed53e226ebe9a59dcb071db
Manu <ominuka.mauro@gmail.com>
2022-06-12 01:19:41 -0300

    Fix: remove fields unncesaries in getById and getAll

--- repositories/events.js
+++ repositories/events.js
@@ -1,10 +1,10 @@
 import Events from '../models/events.js'
 
 const getById = async (id) => {
-  return await Events.findById(id)
+  return await Events.findById(id, '-deleted -deletedAt -createdAt -updatedAt')
 }
 const update = async (id, event) => {
-  return await Events.findByIdAndUpdate(id, event)
+  return await Events.findByIdAndUpdate(id, event, { new: true })
 }
 
 const updateSubdocument = async ({ _id, subDocID }, field, body) => {
@@ -19,7 +19,9 @@ const create = async (event) => {
   return await Events.create(event)
 }
 const getAll = async () => {
-  return await Events.find({}).populate('image_id')
+  return await Events.find({}, '-deleted -deletedAt -createdAt -updatedAt')
+    .populate('image_id')
+    .lean()
 }
 const remove = async (id) => {
   return await Events.findByIdAndRemove(id)
