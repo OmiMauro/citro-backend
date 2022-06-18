3d81c0b6a28f6b462a8077561b912cf641600bdc
Manu <ominuka.mauro@gmail.com>
2022-06-17 23:49:22 -0300

    Remove entity activities

--- controllers/activities.js
+++
@@ -1,9 +0,0 @@
-import activitiesServices from '../services/activities.js'
-
-const create = async (req, res) => { }
-const update = async (req, res) => {}
-const remove = async (req, res) => {}
-const getAll = async (req, res) => {}
-const getById = async (req, res) => { }
-
-export default { create, update, remove, getAll, getById }
