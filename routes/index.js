d954d5f88947d76609e28257a3f2b6ffa280c166
Manu <ominuka.mauro@gmail.com>
2022-06-18 00:39:25 -0300

    Remove routes /activities and /news

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,21 @@
 import Express from 'express'
 
-import activities from './activities.js'
 import auth from './auth.js'
 import cars from './cars.js'
 import galery from './galery.js'
 import inscriptions from './inscriptions.js'
 import members from './members.js'
-import news from './news.js'
 import organizations from './organization.js'
 import slides from './slides.js'
 import users from './users.js'
 import events from './events.js'
 const indexRouter = Express.Router()
 
-indexRouter.use('/activities', activities)
 indexRouter.use('/auth', auth)
 indexRouter.use('/cars', cars)
 indexRouter.use('/galery', galery)
 indexRouter.use('/inscriptions', inscriptions)
 indexRouter.use('/members', members)
-indexRouter.use('/news', news)
 indexRouter.use('/organizations', organizations)
 indexRouter.use('/slides', slides)
 indexRouter.use('/events', events)
