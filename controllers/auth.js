6f111c4601d042f9e42c0a40914d64a32b14fb59
Manu <ominuka.mauro@gmail.com>
2022-06-18 22:35:18 -0300

    Added /me with AuthMe

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,107 +3,118 @@ import config from '../config/config.js'
 import authServices from '../services/auth.js'
 
 const register = async (req, res, next) => {
-	try {
-		const user = await authServices.register(req.body)
-		res.status(201).json({ msg: 'El usuario se creó con exito', data: user })
-	} catch (error) {
-		res
-			.status(error.status)
-			.json({ errors: [{ msg: error.message }], data: { ok: false } })
-	}
+  try {
+    const user = await authServices.register(req.body)
+    res.status(201).json({ msg: 'El usuario se creó con exito', data: user })
+  } catch (error) {
+    res
+      .status(error.status)
+      .json({ errors: [{ msg: error.message }], data: { ok: false } })
+  }
 }
 const login = async (req, res, next) => {
-	try {
-		const { token, user } = await authServices.login(req.body)
-		return res.status(200).json({
-			msg: 'Se inicio sesion con exito',
-			data: { ok: true, token, user }
-		})
-	} catch (error) {
-		return res.status(error.status).json({
-			errors: [{ msg: error.message }],
-			data: { ok: false }
-		})
-	}
+  try {
+    const { token, user } = await authServices.login(req.body)
+    return res.status(200).json({
+      msg: 'Se inicio sesion con exito',
+      data: { ok: true, token, user },
+    })
+  } catch (error) {
+    return res.status(error.status).json({
+      errors: [{ msg: error.message }],
+      data: { ok: false },
+    })
+  }
 }
-const me = async (req, res, next) => {}
+const me = async (req, res) => {
+  try {
+    return res.status(200).json({
+      data: req.authUser,
+    })
+  } catch (error) {
+    return res.status(error.status).json({
+      errors: [{ msg: error.message }],
+      data: { ok: false },
+    })
+  }
+}
 const getAll = async (req, res, next) => {
-	try {
-		const users = await authServices.getAll()
-		return res.status(200).json({ data: users })
-	} catch (error) {
-		return res.status(error.status).json({
-			errors: [{ msg: error.message }],
-			data: { ok: false }
-		})
-	}
+  try {
+    const users = await authServices.getAll()
+    return res.status(200).json({ data: users })
+  } catch (error) {
+    return res.status(error.status).json({
+      errors: [{ msg: error.message }],
+      data: { ok: false },
+    })
+  }
 }
 const forgotPassword = async (req, res, next) => {
-	try {
-		const response = await authServices.forgotPassword(req.body)
-		return res.status(200).json({
-			msg: 'Hemos enviado un email con las instrucciones. Por favor, verifique su casilla de email. ',
-			data: { ok: true }
-		})
-	} catch (error) {
-		return res.status(error.status).json({
-			errors: [{ msg: error.message }],
-			data: { ok: false }
-		})
-	}
+  try {
+    const response = await authServices.forgotPassword(req.body)
+    return res.status(200).json({
+      msg: 'Hemos enviado un email con las instrucciones. Por favor, verifique su casilla de email. ',
+      data: { ok: true },
+    })
+  } catch (error) {
+    return res.status(error.status).json({
+      errors: [{ msg: error.message }],
+      data: { ok: false },
+    })
+  }
 }
 
 const verifyToken = async (req, res, next) => {
-	try {
-		const response = await authServices.isValidToken(req.params.token)
-		return res.status(200).json({
-			msg: 'Token valido',
-			data: { ok: true }
-		})
-	} catch (error) {
-		return res.status(error.status).json({
-			errors: [{ msg: error.message }],
-			data: { ok: false }
-		})
-	}
+  try {
+    const response = await authServices.isValidToken(req.params.token)
+    return res.status(200).json({
+      msg: 'Token valido',
+      data: { ok: true },
+    })
+  } catch (error) {
+    return res.status(error.status).json({
+      errors: [{ msg: error.message }],
+      data: { ok: false },
+    })
+  }
 }
 const resetPassword = async (req, res, next) => {
-	try {
-		const response = await authServices.resetPassword(
-			req.params.token,
-			req.body
-		)
-		return res.status(200).json({
-			msg: 'Su contraseña fue reestablecida con éxito.',
-			data: { ok: true }
-		})
-	} catch (error) {
-		return res.status(error.status).json({
-			errors: [{ msg: error.message }],
-			data: { ok: false }
-		})
-	}
+  try {
+    const response = await authServices.resetPassword(
+      req.params.token,
+      req.body
+    )
+    return res.status(200).json({
+      msg: 'Su contraseña fue reestablecida con éxito.',
+      data: { ok: true },
+    })
+  } catch (error) {
+    return res.status(error.status).json({
+      errors: [{ msg: error.message }],
+      data: { ok: false },
+    })
+  }
 }
 const confirmAccount = async (req, res, next) => {
-	try {
-		const response = await authServices.confirmEmail(req.params.token)
-		return res
-			.status(200)
-			.json({ msg: 'Su cuenta fue verificada con éxito', data: { ok: true } })
-	} catch (error) {
-		return res.status(error.status).json({
-			errors: [{ msg: error.message }],
-			data: { ok: false }
-		})
-	}
+  try {
+    const response = await authServices.confirmEmail(req.params.token)
+    return res
+      .status(200)
+      .json({ msg: 'Su cuenta fue verificada con éxito', data: { ok: true } })
+  } catch (error) {
+    return res.status(error.status).json({
+      errors: [{ msg: error.message }],
+      data: { ok: false },
+    })
+  }
 }
 export default {
-	register,
-	login,
-	me,
-	getAll,
-	forgotPassword,
-	resetPassword,
-	verifyToken,
-	confirmAccount
+  register,
+  login,
+  me,
+  getAll,
+  forgotPassword,
+  resetPassword,
+  verifyToken,
+  confirmAccount,
 }
