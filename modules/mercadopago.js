cab5ae91dc6af9d67b40d98d1ebf6c4cdcb2be17
Manu <ominuka.mauro@gmail.com>
2022-07-10 16:23:54 -0300

    Feat: Add module for manage request to MercadoPago

--- modules/mercadopago.js
+++ modules/mercadopago.js
@@ -5,4 +5,19 @@ mercadopago.configure({
   access_token: config.mercadopago.access_token,
 })
 
-export default mercadopago
+const createPreference = (preference) => {
+  return mercadopago.preferences.create({
+    ...preference,
+    notification_url: `${config.nameApp.api}/orders/webhook?source_news=webhooks`,
+    back_urls: {
+      success: `${config.nameApp.app}`,
+      pending: `${config.nameApp.app}`,
+      failure: `${config.nameApp.app}`,
+    },
+  })
+}
+const getPayment = async (id) => {
+  const response = await mercadopago.payment.get(id)
+  return response
+}
+export default { createPreference, getPayment }
