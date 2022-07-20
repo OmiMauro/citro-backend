a37b018514abb7bf2befdbe49404df95b9bb8a40
Manu <ominuka.mauro@gmail.com>
2022-07-19 22:15:43 -0300

    Fix: remove password when admin recovery all users registers from db

--- repositories/users.js
+++ repositories/users.js
@@ -1,19 +1,19 @@
 import { Users } from '../models/users.js'
 
 const getById = async (id) => {
-	return await Users.findById(id).populate('image_id')
+  return await Users.findById(id).populate('image_id')
 }
 const update = async (id, user) => {
-	return await Users.findByIdAndUpdate(id, user)
+  return await Users.findByIdAndUpdate(id, user)
 }
 const create = async (user) => {
-	return await Users.create(user)
+  return await Users.create(user)
 }
 const getAll = async () => {
-	return await Users.find({}).populate('image_id')
+  return await Users.find({}, '-password').populate('image_id')
 }
 const getByEmail = async (email) => {
-	const exists = await Users.exists({ email })
-	return exists ? await Users.findById(exists._id) : null
+  const exists = await Users.exists({ email })
+  return exists ? await Users.findById(exists._id) : null
 }
 export default { getById, update, create, getAll, getByEmail }
