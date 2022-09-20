dd1a141761c5099b88adc5fec47e053fa7a749f6
Manu <ominuka.mauro@gmail.com>
2022-09-20 20:16:45 -0300

    Remove method for normalizeEmail

--- middlewares/common-validation.js
+++ middlewares/common-validation.js
@@ -3,69 +3,68 @@ import { check, param } from 'express-validator'
 const id = param('id', 'Ingresar el id').exists().isMongoId()
 const id_number = param('id', 'Debe ingresar el id de la organizacion').isInt()
 const name = check('name')
-	.exists()
-	.withMessage('El nombre es requerido')
-	.isString()
-	.withMessage('Tiene que ser caracteres')
-	.isLength({ min: 1, max: 100 })
-	.withMessage('Debe tener entre 1 y 100 caracteres')
+  .exists()
+  .withMessage('El nombre es requerido')
+  .isString()
+  .withMessage('Tiene que ser caracteres')
+  .isLength({ min: 1, max: 100 })
+  .withMessage('Debe tener entre 1 y 100 caracteres')
 
 const lastname = check('lastname')
-	.exists()
-	.withMessage('El apellido es requerido')
-	.isString()
-	.withMessage('Tiene que ser caracteres')
-	.isLength({ min: 1, max: 100 })
-	.withMessage('Debe tener entre 1 y 100 caracteres')
+  .exists()
+  .withMessage('El apellido es requerido')
+  .isString()
+  .withMessage('Tiene que ser caracteres')
+  .isLength({ min: 1, max: 100 })
+  .withMessage('Debe tener entre 1 y 100 caracteres')
 
 const image = check('image')
-	.if((value, { req }) => !req.file)
-	.isString()
-	.withMessage('Debe ser una URL')
-	.isLength({ min: 1, max: 1234 })
-	.withMessage('Longitud entre: 1-1234')
+  .if((value, { req }) => !req.file)
+  .isString()
+  .withMessage('Debe ser una URL')
+  .isLength({ min: 1, max: 1234 })
+  .withMessage('Longitud entre: 1-1234')
 
 const email = check('email')
-	.exists()
-	.withMessage('Debe ingresar el email')
-	.isEmail()
-	.withMessage('Debe ingresar un email valido')
-	.normalizeEmail()
+  .exists()
+  .withMessage('Debe ingresar el email')
+  .isEmail()
+  .withMessage('Debe ingresar un email valido')
 
 const phone = check('phone')
-	.isString()
-	.withMessage('Ingrese un numero de celular valido')
+  .isString()
+  .withMessage('Ingrese un numero de celular valido')
 
 const urlFacebook = check('urlfacebook')
-	.optional()
-	.isString()
-	.withMessage('Debe ingresar caracteres')
-	.isURL()
-	.withMessage('Debe ingresar una URL')
+  .optional()
+  .isString()
+  .withMessage('Debe ingresar caracteres')
+  .isURL()
+  .withMessage('Debe ingresar una URL')
 
 const urlInstragram = check('urlInstragram')
-	.optional()
-	.isString()
-	.withMessage('Debe ingresar caracteres')
-	.isURL()
-	.withMessage('Debe ingresar una URL')
+  .optional()
+  .isString()
+  .withMessage('Debe ingresar caracteres')
+  .isURL()
+  .withMessage('Debe ingresar una URL')
 
 const urlWhatsapp = check('urlWhatsapp')
-	.optional()
-	.isString()
-	.withMessage('Debe ingresar caracteres')
-	.isURL()
-	.withMessage('Debe ingresar una URL')
+  .optional()
+  .isString()
+  .withMessage('Debe ingresar caracteres')
+  .isURL()
+  .withMessage('Debe ingresar una URL')
 
 export {
-	name,
-	lastname,
-	id,
-	id_number,
-	image,
-	email,
-	phone,
-	urlFacebook,
-	urlInstragram,
-	urlWhatsapp
+  name,
+  lastname,
+  id,
+  id_number,
+  image,
+  email,
+  phone,
+  urlFacebook,
+  urlInstragram,
+  urlWhatsapp,
 }
