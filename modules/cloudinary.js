9fb2681db1716cc5a915b1870fa572c6661f2d0f
Manu <ominuka.mauro@gmail.com>
2022-09-28 15:07:20 -0300

    Fix: add sign ? for image?.public_id

--- modules/cloudinary.js
+++ modules/cloudinary.js
@@ -5,40 +5,36 @@ import imagesRepository from '../repositories/images.js'
 const { cloudinary } = config
 
 v2.config({
-	cloud_name: cloudinary.cloud_name,
-	api_key: cloudinary.api_key,
-	api_secret: cloudinary.api_secret
+  cloud_name: cloudinary.cloud_name,
+  api_key: cloudinary.api_key,
+  api_secret: cloudinary.api_secret,
 })
 
 const uploadFile = async (file, folder = 'citros') => {
-	try {
-		/* const fileStream = fs.createReadStream(file.path)
-    // const fileName = file.split('/')[3] + file.ext
-    const fileContenType = file.contentType */
-		const { path } = file
+  try {
+    const { path } = file
 
-		const response = await v2.uploader.upload(path, { folder })
-		return response
-	} catch (error) {
-		console.log(error)
-	}
+    const response = await v2.uploader.upload(path, { folder })
+    return response
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const deleteFile = async (image_id) => {
-	try {
-		const image = await imagesRepository.getById(image_id)
-		if (!image) {
-			const error = new Error('No se encontro una imagen con el ID')
-			error.status = 404
-			throw error
-		}
-		return await v2.uploader.destroy(image.public_id)
-	} catch (error) {
-		console.log(error)
-		const err = new Error(error.message)
-		err.status = 500
-		throw err
-	}
+  try {
+    const image = await imagesRepository.getById(image_id)
+    if (!image) {
+      const error = new Error('No se encontro una imagen con el ID')
+      error.status = 404
+      throw error
+    }
+    return await v2.uploader.destroy(image?.public_id)
+  } catch (error) {
+    const err = new Error(error.message)
+    err.status = 500
+    throw err
+  }
 }
 
 export default { uploadFile, deleteFile }
