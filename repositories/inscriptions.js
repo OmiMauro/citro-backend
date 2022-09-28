a1389f7769fa0c4c51f6f44e36bd98bd7bb1879d
Manu <ominuka.mauro@gmail.com>
2022-09-28 15:04:21 -0300

    Fix: populate data from event

--- repositories/inscriptions.js
+++ repositories/inscriptions.js
@@ -1,7 +1,10 @@
 import Inscriptions from '../models/inscriptions.js'
 
 const getById = async (id) => {
-  return await Inscriptions.findById(id).populate('_orderId').lean()
+  return await Inscriptions.findById(id)
+    .populate('_orderId')
+    .populate('_eventId')
+    .lean()
 }
 const update = async (id, inscription) => {
   return await Inscriptions.findByIdAndUpdate(id, inscription, { new: true })
